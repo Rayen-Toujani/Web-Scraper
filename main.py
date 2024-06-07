8dbe1d2da6d0b92a7af78c87222fa05a8a4cfd5d
Mohamed Rayen Toujani <mo.rayen.toujani@gmail.com>
2024-06-07 23:25:25 +0100

    getting the name and weight of the protein product and put them in a file

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ url = "https://protein-shop-tunisia.tn/categorie-produit/nutrition-sportive/gain
 
 response = requests.get(url)
 
-if response.status_code==200:
-    soup = BeautifulSoup(response.content,'html.parser')
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
 
     Prot_Names = []
+
+    for prot in soup.find_all('h3'):
+        # Extract the text from the <a> tag within the <h3> tag
+        isem_prot = prot.find('a').get_text()
+        Prot_Names.append(isem_prot)
+
+    for idx, isem_prot in enumerate(Prot_Names):
+        print(f"{idx + 1}. {isem_prot}")
+
+else:
+    print("Failed to fetch")
